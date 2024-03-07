caddbc6d230a6a4d60395e8dbc24aa99aef83a3f
HuseynliTogrul <toghrulih@code.edu.az>
2024-03-07 10:57:44 +0400

    Nest AspNet task

---
+++ Nest-AspNet/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Nest_AspNet.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public double Rating { get; set; }
+        public decimal SellPrice { get; set; } = default!;
+        public decimal DiscountPrice { get; set; }
+    }
+}
---
+++ Nest-AspNet/Models/ProductImage.cs
@@ -0,0 +1,9 @@
+﻿namespace Nest_AspNet.Models
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public string Url { get; set; } = null!;
+        public IFormFile File { get; set; }
+    }
+}
